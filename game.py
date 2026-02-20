989a5c6225f38a856698a15805324eae6524f924
akpradhan04 <pradhan.adityak@gmail.com>
2026-02-20 21:44:39 +0545

    Added UI elements

--- game.py
+++ game.py
@@ -9,8 +9,12 @@ class CountriesGame:
         self.root = tk.Tk()
         self.root.title(config.WINDOW_TITLE)
 
+        self.start_time = time.time()
+
         self.countries_data = self.load_countries()
         self.countries_lookup = self.build_lookup()
+
+        self.setup_ui()
 
         self.root.mainloop()
 
@@ -32,6 +36,24 @@ class CountriesGame:
                 countries_lookup[alias.lower()] = accepted_country_name
 
         return countries_lookup
+    
+    def setup_ui(self):
+        self.counter_label = tk.Label(
+            self.root,
+            text=f"0 / {len(self.countries_data)}",
+            font=("Arial", 16)
+        )
+        self.counter_label.pack()
+
+        self.timer_label = tk.Label(self.root, font=("Arial", 16))
+        self.timer_label.pack()
+
+        self.entry = tk.Entry(self.root, font=("Arial", 14), width=40)
+        self.entry.pack()
+        self.entry.focus()
+
+        self.listbox = tk.Listbox(self.root, width=50, height=20)
+        self.listbox.pack()
 
 
 if __name__ == "__main__":
